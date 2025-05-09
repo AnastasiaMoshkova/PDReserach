0ae792773606936b5bc6b190bddfe870edcaf71a
AnastasiaMoshkova <nast.moshkova@yandex.ru>
2025-05-09 22:25:17 +0300

    изменен подход к tremor processing и feature extraction, добавлены новые фичи

--- data_base/tremor.py
+++ data_base/tremor.py
@@ -1,30 +1,13 @@
 import json
-import numpy as np
-from scipy import fftpack
-from statsmodels.nonparametric.smoothers_lowess import lowess
-from scipy import loadtxt, optimize
-from scipy.signal import argrelmax
-from scipy.signal import argrelmin
-from scipy.signal import medfilt
-from scipy.signal import savgol_filter
-from pylab import *
-# import pywt
-from scipy.fftpack import rfft, irfft, fftfreq
 import os
-import glob
-import numpy
+import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
-import math
-import scipy as sp
-import matplotlib.pyplot as plt
-# пострроение спектра сигнала
-from scipy import fftpack
-from scipy.fftpack import rfft, irfft, fftfreq
-from scipy.signal import argrelmax
-import scipy as sp
-import matplotlib.pyplot as plt
+from scipy import signal
+from scipy.signal import medfilt
+from scipy.signal import  hilbert, butter, lfilter
 
-
+TIMESTAMP_COEFFICIENT = 1000000
 class TremorProcessing:
     '''
     Objective:
@@ -43,7 +26,7 @@ class TremorProcessing:
         frame: массив с номерами кадров
     '''
 
-    def _tremor_signal(self, file, hand_type, key_point):
+    def _tremor_signal(self, file, hand_type, key_point, start, stop):
         hands = {'R': 'right hand', 'L': 'left hand'}
         hand = hands[hand_type]
         raw_data = json.load(open(file))
@@ -61,46 +44,83 @@ class TremorProcessing:
                 X.append(float(data[i][hand][key_point]['X1']))
                 Y.append(float(data[i][hand][key_point]['Y1']))
                 Z.append(float(data[i][hand][key_point]['Z1']))
-                frame.append(data[i]['frame'])
+                frame.append(data[i][hand]['info']['timestamp'])
+        frame = [(fr - frame[0])/TIMESTAMP_COEFFICIENT for fr in frame]
+        frame = np.array(frame)
+
+        X = np.array(X)[(frame < stop) & (frame > start)].tolist()
+        Y = np.array(Y)[(frame < stop) & (frame > start)].tolist()
+        Z = np.array(Z)[(frame < stop) & (frame > start)].tolist()
+        frame = frame[(frame < stop) & (frame > start)].tolist()
+
+        df = pd.DataFrame({'X':X, 'Y':Y, 'Z':Z, 'frame':frame})
+
+        if len(X) > 0:
+            X = signal.detrend(X)
+            Y = signal.detrend(Y)
+            Z = signal.detrend(Z)
+
+            k = 21
+            smoothed_x = self.meanfilt(X, k)
+            smoothed_y = self.meanfilt(Y, k)
+            smoothed_z = self.meanfilt(Z, k)
+
+            # smoothed_x = ndimage.median_filter(x, size=20) # median_filter
+            # smoothed_y = ndimage.median_filter(y, size=20)
+            X = X - smoothed_x
+            Y = Y - smoothed_y
+            Z = Z - smoothed_z
+
         return X, Y, Z, frame
 
-    # функция принимает frame - массив с номерами адров , znach - какой либо из массивов с координатами X,Y или Z
-    # функция возвращает pointY - координату точки по Y (красные точки на графике - огибающая спектра),
-    # pointX - координату точки по X (красные точки на графике - огибающая спектра)
-    #  maxTemp - массив точек pointY, pointX - для построения огибающей,
-    # f_signal - массив со значениями гармоник спектр сигнала, ff - массив со значеними частот, fn - граничный отсчет в массиве частот
 
-    '''
-    Objective:
+    # from https://github.com/peach-lucien/PoET/blob/main/PoET/features.py
+    def meanfilt(self, x, k):
+        """Apply a length-k mean filter to a 1D array x.
+        Boundaries are extended by repeating endpoints.
+        """
 
-    Inputs:
-        frame: массив с номерами кадров
-        values: какой либо из массивов с координатами X,Y или Z
+        assert k % 2 == 1, "Mean filter length must be odd."
+        assert x.ndim == 1, "Input must be one-dimensional."
 
-    Output:
-        pointY: координату точки по Y (красные точки на графике - огибающая спектра)
-        pointX: координату точки по X (красные точки на графике - огибающая спектра)
-        maxTemp: массив точек pointY, pointX - для построения огибающей
-        f_signal: массив со значениями гармоник спектр сигнала
-        ff: массив со значеними частот
-        fn: граничный отсчет в массиве частот
-    '''
+        k2 = (k - 1) // 2
+        y = np.zeros((len(x), k), dtype=x.dtype)
+        y[:, k2] = x
+        for i in range(k2):
+            j = k2 - i
+            y[j:, i] = x[:-j]
+            y[:j, i] = x[0]
+            y[:-j, -(i + 1)] = x[j:]
+            y[-j:, -(i + 1)] = x[-1]
+        return np.mean(y, axis=1)
 
-    def _spectrXYZ(self, frame, values):
-        time = np.array(frame) / 100
-        signal = np.array(values)
-        W = fftfreq(signal.size, d=time[1] - time[0])
-        f_signal = rfft(signal)
-        fn = 800
-        ff = list(abs(W[1:fn] / 2))
-        maxTempnp = argrelmax(abs(f_signal[1:fn - 1]), order=5)
-        pointY = []
-        pointX = []
-        maxTemp = maxTempnp[0].tolist()
-        for j in range(len(maxTemp)):
-            pointY.append(abs(f_signal[1:fn - 1])[maxTemp[j]])
-            pointX.append((ff[1:fn - 1])[maxTemp[j]])
-        return pointY, pointX, maxTemp, f_signal, ff, fn
+    # from https://github.com/peach-lucien/PoET/blob/main/PoET/features.py
+    def butter_bandpass(self, lowcut, highcut, fs, order=5):
+        return butter(order, [lowcut, highcut], fs=fs, btype='band')
+
+    # from https://github.com/peach-lucien/PoET/blob/main/PoET/features.py
+    def butter_bandpass_filter(self, data, lowcut, highcut, fs, order=5):
+        b, a = self.butter_bandpass(lowcut, highcut, fs, order=order)
+        y = lfilter(b, a, data)
+        return y
+
+    # from https://github.com/peach-lucien/PoET/blob/main/PoET/features.py
+    def _spectrum(self, frame, y):
+
+        time = np.array(frame)
+
+        fft_result = np.fft.fft(y)
+        frequency = np.fft.fftfreq(y.size, d= time[1] - time[0])
+
+        # Calculate the magnitude of the FFT (amplitude spectrum)
+        amplitude = 2 * np.abs(fft_result) / len(
+            y)  # Multiplying by 2 because the spectrum is symmetrical for real-valued signals
+
+        # positive frequencies only
+        amplitude = amplitude[frequency >= 0]
+        frequency = frequency[frequency >= 0]
+
+        return amplitude, frequency
 
     '''
     Objective:
@@ -114,13 +134,43 @@ class TremorProcessing:
     '''
 
     def _plot_tremor_signals_and_spectrum(self, frame, X, Y, Z, name, output_path):
+        X = np.array(X) - np.min(X)
+        Y = np.array(Y) - np.min(Y)
+        Z = np.array(Z) - np.min(Z)
 
-        X = np.array(X) - min(X)
-        Y = np.array(Y) - min(Y)
-        Z = np.array(Z) - min(Z)
-        pointY_1, pointX_1, maxTemp_1, f_signal_1, ff_1, fn = self._spectrXYZ(frame, X)
-        pointY_2, pointX_2, maxTemp_2, f_signal_2, ff_2, fn = self._spectrXYZ(frame, Y)
-        pointY_3, pointX_3, maxTemp_3, f_signal_3, ff_3, fn = self._spectrXYZ(frame, Z)
+        Xfiltered = self.butter_bandpass_filter(X, 3, 12, 1/(frame[1] - frame[0]), order=5)
+        Yfiltered = self.butter_bandpass_filter(Y, 3, 12, 1 / (frame[1] - frame[0]), order=5)
+        Zfiltered = self.butter_bandpass_filter(Z, 3, 12, 1 / (frame[1] - frame[0]), order=5)
+
+        aX, fX = self._spectrum(frame, Xfiltered)
+        aY, fY = self._spectrum(frame, Yfiltered)
+        aZ, fZ = self._spectrum(frame, Zfiltered)
+
+        ylim = 0.4
+        ylimfrq = 15
+        fig = plt.figure(figsize=(15, 11))
+        ax_1 = fig.add_subplot(3, 1, 1)
+        ax_1.plot(fX, aX)
+        ax_1.legend('X')
+        ax_1.set_xlim(0, ylimfrq)
+        ax_1.set_ylim(0, ylim)
+
+        ax_2 = fig.add_subplot(3, 1, 2)
+        ax_2.plot(fY, aY)
+        ax_2.legend('Y')
+        ax_2.set_xlim(0, ylimfrq)
+        ax_2.set_ylim(0, ylim)
+
+        ax_3 = fig.add_subplot(3, 1, 3)
+        ax_3.plot(fZ, aZ)
+        ax_3.legend('Z')
+        ax_3.set_xlim(0, ylimfrq)
+        ax_3.set_ylim(0, ylim)
+
+        ax_3.set_xlabel("Частота (Гц)")
+        plt.savefig(os.path.join(output_path, name+'spectrum.png'))
+        #plt.show()
+        plt.clf()
 
         SMALL_SIZE = 15
         MEDIUM_SIZE = 10
@@ -139,53 +189,26 @@ class TremorProcessing:
 
         fig = plt.figure(figsize=(15, 11))
         ax_1 = fig.add_subplot(3, 1, 1)
-        ax_1.plot(np.array(frame) / 100, X)
+        ax_1.plot(np.array(frame), X)
         ax_1.legend('X')
         ax_1.set_ylabel("Амплитуда (мм)")
         ax_1.set_xlim(-1, 20)
 
         ax_2 = fig.add_subplot(3, 1, 2)
-        ax_2.plot(np.array(frame) / 100, Y)
+        ax_2.plot(np.array(frame), Y)
         ax_2.legend('Y')
         ax_2.set_ylabel("Амплитуда (мм)")
         ax_2.set_xlim(-1, 20)
 
         ax_3 = fig.add_subplot(3, 1, 3)
-        ax_3.plot(np.array(frame) / 100, Z)
+        ax_3.plot(np.array(frame), Z)
         ax_3.legend('Z')
         ax_3.set_xlabel("Время (с)")
         ax_3.set_ylabel("Амплитуда (мм)")
         ax_3.set_xlim(-1, 20)
 
-        # plt.savefig(os.path.join(output_path, name + '_signal.png'))
-        plt.show()
-        plt.clf()
-
-        fig = plt.figure(figsize=(15, 11))
-        ax_1 = fig.add_subplot(3, 1, 1)
-        ax_1.plot(ff_1[1:fn - 1], abs(f_signal_1[1:fn - 1]))
-        ax_1.legend('X')
-        ax_1.plot(pointX_1, pointY_1, color='black')
-        for j in range(len(maxTemp_1)):
-            ax_1.plot((ff_1[1:fn - 1])[maxTemp_1[j]], abs(f_signal_1[1:fn - 1])[maxTemp_1[j]], 'ro')
-
-        ax_2 = fig.add_subplot(3, 1, 2)
-        ax_2.plot(ff_2[1:fn - 1], abs(f_signal_2[1:fn - 1]))
-        ax_2.legend('Y')
-        ax_2.plot(pointX_2, pointY_2, color='black')
-        for j in range(len(maxTemp_2)):
-            ax_2.plot((ff_2[1:fn - 1])[maxTemp_2[j]], abs(f_signal_2[1:fn - 1])[maxTemp_2[j]], 'ro')
-
-        ax_3 = fig.add_subplot(3, 1, 3)
-        ax_3.plot(ff_3[1:fn - 1], abs(f_signal_3[1:fn - 1]))
-        ax_3.legend('Z')
-        ax_3.plot(pointX_3, pointY_3, color='black')
-        for j in range(len(maxTemp_3)):
-            ax_3.plot((ff_3[1:fn - 1])[maxTemp_3[j]], abs(f_signal_3[1:fn - 1])[maxTemp_3[j]], 'ro')
-
-        ax_3.set_xlabel("Частота (Гц)")
-        # plt.savefig(os.path.join(output_path, name+'.png')
-        plt.show()
+        plt.savefig(os.path.join(output_path, name + '_signal.png'))
+        #plt.show()
         plt.clf()
 
     '''
@@ -201,54 +224,65 @@ class TremorProcessing:
         ampl: значение амлитуды максимальной гармоники в промежутки от 3 до 6 Гц
 
     '''
-    def _calculate_features(self, X, Y, Z, frame):
-        X = np.array(X) - min(X)
-        Y = np.array(Y) - min(Y)
-        Z = np.array(Z) - min(Z)
-        pointY_X, pointX_X, maxTemp_X, f_signal_X, ff_X, fn = self._spectrXYZ(frame, X)
-        pointY_Y, pointX_Y, maxTemp_Y, f_signal_Y, ff_Y, fn = self._spectrXYZ(frame, Y)
-        pointY_Z, pointX_Z, maxTemp_Z, f_signal_Z, ff_Z, fn = self._spectrXYZ(frame, Z)
 
-        ff36_X = []
-        ff36_Y = []
-        ff36_Z = []
+    def _calculate_features(self, X, Y, Z, frame, f1, f2, quality):
 
-        for j in range(len(ff_X)-1):
-            if ((ff_X[j] > 3) & (ff_X[j] < 6)):
-                ff36_X.append(abs(f_signal_X[1:fn - 1])[j])
-        for j in range(len(ff_Y)-1):
-            if ((ff_Y[j] > 3) & (ff_Y[j] < 6)):
-                ff36_Y.append(abs(f_signal_Y[1:fn - 1])[j])
-        for j in range(len(ff_Z)-1):
-            if ((ff_Z[j] > 3) & (ff_Z[j] < 6)):
-                ff36_Z.append(abs(f_signal_Z[1:fn - 1])[j])
+        if ((len(X)==0) | (quality == 0)):
+            return {'Fmean': np.NaN,
+                    'Amean': np.NaN,
+                    'Fmax': np.NaN,
+                    'Amax': np.NaN,
+                    'PWmean': np.NaN,
+                    'PWmax': np.NaN,
+                    }
 
-        # print(np.where(abs(f_signal_1[1:fn - 1])==max(abs(f_signal_1[1:fn - 1])))[0][0])
+        Xfiltered = self.butter_bandpass_filter(X, f1, f2, 1 / (frame[1] - frame[0]), order=5)
+        Yfiltered = self.butter_bandpass_filter(Y, f1, f2, 1 / (frame[1] - frame[0]), order=5)
+        Zfiltered = self.butter_bandpass_filter(Z, f1, f2, 1 / (frame[1] - frame[0]), order=5)
 
-        ffmax_X = ff_X[np.where(abs(f_signal_X[1:fn - 1]) == max(abs(f_signal_X[1:fn - 1])))[0][0]]
-        ffmax_Y = ff_Y[np.where(abs(f_signal_Y[1:fn - 1]) == max(abs(f_signal_Y[1:fn - 1])))[0][0]]
-        ffmax_Z = ff_Z[np.where(abs(f_signal_Z[1:fn - 1]) == max(abs(f_signal_Z[1:fn - 1])))[0][0]]
+        aX, fX = self._spectrum(frame, Xfiltered)
+        aY, fY = self._spectrum(frame, Yfiltered)
+        aZ, fZ = self._spectrum(frame, Zfiltered)
 
-        f_mean_XYZ, ampl_mean_XYZ = np.mean([ffmax_X, ffmax_Y, ffmax_Z]), np.mean(
-            [max(ff36_X), max(ff36_Y), max(ff36_Z)])
-        f_max_XYZ, ampl_max_XYZ = np.max([ffmax_X, ffmax_Y, ffmax_Z]), np.max([max(ff36_X), max(ff36_Y), max(ff36_Z)])
+        fX, fY, fZ = np.array(fX), np.array(fY), np.array(fZ),
+        amax_X, amax_Y, amax_Z = np.max(aX), np.max(aY), np.max(aZ)
+        ffmax_X, ffmax_Y, ffmax_Z = fX[aX==amax_X][0], fY[aY==amax_Y][0], fZ[aZ==amax_Z][0]
 
-        return {'Fmean': f_mean_XYZ, 'Amean': ampl_mean_XYZ, 'Fmax': f_max_XYZ, 'Amax': ampl_max_XYZ}
+        f_mean_XYZ, ampl_mean_XYZ = (np.mean([ffmax_X, ffmax_Y, ffmax_Z]),
+                                     np.mean([amax_X, amax_Y, amax_Z]))
+        f_max_XYZ, ampl_max_XYZ = (np.max([ffmax_X, ffmax_Y, ffmax_Z]),
+                                   np.max([amax_X, amax_Y, amax_Z]))
 
-    def get_features(self, path, file, hand, key_point):
-        X, Y, Z, frame = self._tremor_signal(os.path.join(path, file), hand, key_point)
-        return self._calculate_features(X, Y, Z, frame)
+        pwX, pwY, pwZ = np.mean(np.array(aX) ** 2), np.mean(np.array(aY) ** 2), np.mean(np.array(aZ) ** 2)
+        pw_mean = np.mean([pwX, pwY, pwZ])
+        pw_max = np.max([pwX, pwY, pwZ])
+
+        return {'Fmean': f_mean_XYZ,
+                'Amean': ampl_mean_XYZ,
+                'Fmax': f_max_XYZ,
+                'Amax': ampl_max_XYZ,
+                'PWmean': pw_mean,
+                'PWmax': pw_max}
+
+
+    def get_features(self, path, file, hand, key_point, frq, feature_type, start, stop):
+        X, Y, Z, frame = self._tremor_signal(os.path.join(path, file), hand, key_point, start, stop)
+        quality = self._quality_check(X, Y, Z, file)
+        features = self._calculate_features(X, Y, Z, frame, frq[0], frq[1], feature_type, quality = quality)
+        return features
 
     def plot_tremor(self, path, file, hand, key_point, output_path):
-        X, Y, Z, frame = self._tremor_signal(os.path.join(path, file), hand, key_point)
-        self._plot_tremor_signals_and_spectrum(frame, X, Y, Z, file.split('.')[0], output_path)
+        X, Y, Z, frame = self._tremor_signal(os.path.join(path, file), hand, key_point, 2, 18)
+        if ((len(X)>0) & (len(Y)>0) & (len(Z)>0)):
+            self._plot_tremor_signals_and_spectrum(frame, X, Y, Z, file.split('.')[0] + '_' + hand, output_path)
 
 
-    def _quality_check(self):
-        '''
-        TODO
-        написать функцию которая возращает значения (например 0 и 1),
-        обозначающее пригодность сигнала для дальнейшей обработки
-        Функция _quality_check может быть вызвана в _calculate_features, где например сначала производится проверка качества сигнала,
-        а затем рассчитываются признаки или возвращается NAN (или -1)
-        '''
+    def _quality_check(self, X, Y, Z, file):
+
+        #patient 32, 95 ?
+        #print(file, np.var(X), np.var(Y), np.var(Z))
+        xvar, yvar, zvar  = np.var(X), np.var(Y), np.var(Z)
+
+        threshold = 20
+        return 0 if ((xvar > threshold) | (yvar > threshold) | (zvar > threshold)) else 1
+
